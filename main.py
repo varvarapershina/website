cc2788dfe622a904bfff3d43b85a42a71eed29b2
demonichek <rondo272@yandex.ru>
2024-04-30 23:03:09 +0300

    МОЯ ЧАСТЬ

--- main.py
+++ main.py
@@ -276,3 +276,281 @@ def apteka():
                               transition: .3s linear;
                             }
 
+                            .menu-main a:hover:before {opacity: 1;}
+                            @media (max-width: 660px) {
+                            .menu-main {
+                              float: none;
+                              padding-top: 20px;
+                            }
+
+                            .top-menu {
+                              text-align: center;
+                              padding: 20px 0 0 0;
+                            }
+                            
+                            .menu-main a {padding: 0 10px;}
+                            .menu-main a:before {transform: rotate(45deg) translateX(-6px);}
+                            }
+
+                            @media (max-width: 600px) {
+                            .menu-main li {display: block;}
+                            }
+
+                            .card-container {
+                              display: flex;
+                              flex-wrap: wrap;
+                              justify-content: center;
+                              gap: 16px;
+                              padding: 16px;
+                            }
+
+                            .card {
+                              background-color: AliceBlue;
+                              box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+                              width: 300px;
+                              border-radius: 10px;
+                              overflow: hidden;
+                              padding: 16px;
+                            }
+
+                            .card img {
+                              width: 100%;
+                              height: auto;
+                            }
+
+                            .card h3 {
+                              margin-top: 16px;
+                              margin-bottom: 10px;
+                              font-style: italic;
+                            }
+
+                            .card p {
+                              color: #777;
+                            }
+                        </style>
+
+                    </head>
+                    <body>
+                        <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+                          <symbol id="check-circle-fill" fill="currentColor" viewBox="0 0 16 16">
+                            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+                          </symbol>
+                          <symbol id="info-fill" fill="currentColor" viewBox="0 0 16 16">
+                            <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>
+                          </symbol>
+                          <symbol id="exclamation-triangle-fill" fill="currentColor" viewBox="0 0 16 16">
+                            <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                          </symbol>
+                        </svg>
+                        <nav class="top-menu">
+                          <a class="navbar-logo" href=""><img src="https://yt3.googleusercontent.com/ytc/AOPolaT_Ladu1VMq4fJ6va2dpeQEdg1d9D0WFaUTX5jW=s900-c-k-c0x00ffffff-no-rj" alt="Лого"
+                          style="display: block; width: 115px;"></a>
+                          <ul class="menu-main">
+                            <li><a href="http://127.0.0.1:8080/site_apteka">Главная</a></li>
+                            <li><a href="http://127.0.0.1:8080/work_for_us/">Работа</a></li>
+                            <li><a href="http://127.0.0.1:8080/about_us/">О нас</a></li>
+                          </ul>
+                        </nav>
+                        <form>
+                            <input type="text" placeholder="Поиск по сайту">
+                            <button value="" title="Найти" alt="Найти" class="fa ser-button" type="submit">Найти</button>
+                        </form>
+                        <section class="main-section">
+                            <div class="container">
+                                <h1 class="small-caps">Добро пожаловать, сотрудник!</h1>
+                                <p class="main-subtitle">Дороже лекарств может быть только здоровье...</p>
+                            </div>
+
+                        <div class="card-container">
+                          <div class="card">
+                            <img src="https://citatnica.ru/wp-content/uploads/2019/06/897701440575bfed599a8b0.89490139.jpg" alt="Image 1">
+                            <h3>"Наша медицина двигается
+                            вместе с такими компаниями"</h3>
+                            <p>Ведущий специалист</p>
+                          </div>
+
+                          <div class="card">
+                            <img src="https://fraufluger.ru/wp-content/uploads/2021/04/j_lqasli650.jpg" alt="Image 2">
+                            <h3>"Хороший коллектив сотрудников"</h3>
+                            <p>Сотрудник</p>
+                          </div>
+
+                          <div class="card">
+                            <img src="https://mykaleidoscope.ru/x/uploads/posts/2022-09/1663177004_52-mykaleidoscope-ru-p-deti-v-vostorge-vkontakte-59.jpg" alt="Image 2">
+                            <h3>"Спасибо! Ваше лекарство помогло
+                            Моему ребенку!"</h3>
+                            <p>Покупательница</p>
+                          </div>
+                        </div>
+
+                        </section>
+                        <div class="alert alert-success d-flex align-items-center" role="alert">
+                          <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>
+                          <div>
+                            <h3 class="small-caps">Наша компания «Фармик» предоставляет лекарства для всех больниц города</h3>
+                          </div>
+                        </div>
+                        <h2 class="small-caps">Мы известны такими лекарствами, как:</h2>
+
+                        <div class="container">
+                            <div id="myCarousel" class="carousel slide" data-ride="carousel">
+
+                                <ol class="carousel-indicators">
+                                    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                                    <li data-target="#myCarousel" data-slide-to="1"></li>
+                                    <li data-target="#myCarousel" data-slide-to="2"></li>
+                                    <li data-target="#myCarousel" data-slide-to="3" class="active"></li>
+                                    <li data-target="#myCarousel" data-slide-to="4"></li>
+                                    <li data-target="#myCarousel" data-slide-to="5"></li>
+                                    <li data-target="#myCarousel" data-slide-to="6" class="active"></li>
+                                    <li data-target="#myCarousel" data-slide-to="7"></li>
+                                    <li data-target="#myCarousel" data-slide-to="8"></li>
+                                    <li data-target="#myCarousel" data-slide-to="9"></li>
+                                </ol>
+
+                                <div class="carousel-inner">
+                                    <div class="item active">
+                                        <img src="https://api.biocadless.com/uploads/biocadru/1689602248350.png" alt="Lekarstvo1" style="width:50%;">
+                                        <div class="carousel-caption">
+                                            <h3>Ковада</h3>
+                                            <p>Онкологические заболевания</p>
+                                            <form action="https://biocad.ru/products/autoimmune/tribuvia" target="_blank">
+                                            <button class="btn btn-primary">На сайт...</button>
+                                        </div>
+                                    </div>
+
+                                    <div class="item">
+                                        <img src="https://api.biocadless.com/uploads/biocadru/1685451718152.png" alt="Lekarstvo2" style="width:50%;">
+                                        <div class="carousel-caption">
+                                            <h3>Инфликсимаб</h3>
+                                            <p>Аутоиммунные заболевания</p>
+                                            <form action="https://biocad.ru/products/autoimmune/infliksimab" target="_blank">
+                                            <button class="btn btn-primary">На сайт...</button>
+                                        </div>
+                                    </div>
+
+                                    <div class="item">
+                                        <img src="https://api.biocadless.com/uploads/biocadru/1696842217764.png" alt="Lekarstvo3" style="width:50%;">
+                                        <div class="carousel-caption">
+                                            <h3>Платикад</h3>
+                                            <p>Онкологические заболевания</p>
+                                            <form action="https://biocad.ru/products/onco/platikad" target="_blank">
+                                            <button class="btn btn-primary">На сайт...</button>
+                                        </div>
+                                    </div>
+
+                                    <div class="item">
+                                        <img src="https://api.biocadless.com/uploads/biocadru/1689602248658.png" alt="Lekarstvo4" style="width:50%;">
+                                        <div class="carousel-caption">
+                                            <h3>Пеметрексед</h3>
+                                            <p>Онкологические заболевания</p>
+                                            <form action="https://biocad.ru/products/onco/pemetreksed" target="_blank">
+                                            <button class="btn btn-primary">На сайт...</button>
+                                        </div>
+                                    </div>
+
+                                    <div class="item">
+                                        <img src="https://api.biocadless.com/uploads/biocadru/1696842218256.png" alt="Lekarstvo5" style="width:50%;">
+                                        <div class="carousel-caption">
+                                            <h3>Бортезомиб</h3>
+                                            <p>Онкологические заболевания</p>
+                                            <form action="https://biocad.ru/products/onco/bortezomib" target="_blank">
+                                            <button class="btn btn-primary">На сайт...</button>
+                                        </div>
+                                    </div>
+
+                                    <div class="item">
+                                        <img src="https://api.biocadless.com/uploads/biocadru/1709122602443.png" alt="Lekarstvo6" style="width:50%;">
+                                        <div class="carousel-caption">
+                                            <h3>Ацверис</h3>
+                                            <p>Аутоиммунные заболевания</p>
+                                            <form action="https://biocad.ru/products/autoimmune/acveris" target="_blank">
+                                            <button class="btn btn-primary">На сайт...</button>
+                                        </div>
+                                    </div>
+
+                                    <div class="item">
+                                        <img src="https://api.biocadless.com/uploads/biocadru/1689602545557.png" alt="Lekarstvo7" style="width:50%;">
+                                        <div class="carousel-caption">
+                                            <h3>Гемцитар</h3>
+                                            <p>Онкологические заболевания</p>
+                                            <form action="https://biocad.ru/products/onco/gemcitar" target="_blank">
+                                            <button class="btn btn-primary">На сайт...</button>
+                                        </div>
+                                    </div>
+
+                                    <div class="item">
+                                        <img src="https://api.biocadless.com/uploads/biocadru/1714115735680.png" alt="Lekarstvo8" style="width:50%;">
+                                        <div class="carousel-caption">
+                                            <h3>Трибувиа</h3>
+                                            <p>Аутоиммунные заболевания</p>
+                                            <form action="https://biocad.ru/products/autoimmune/tribuvia" target="_blank">
+                                            <button class="btn btn-primary">На сайт...</button>
+                                        </div>
+                                    </div>
+
+                                    <div class="item">
+                                        <img src="https://api.biocadless.com/uploads/biocadru/1709122602447.png" alt="Lekarstvo9" style="width:50%;">
+                                        <div class="carousel-caption">
+                                            <h3>Нурдати</h3>
+                                            <p>Онкологические заболевания</p>
+                                            <form action="https://biocad.ru/products/onco/nurdati" target="_blank">
+                                            <button class="btn btn-primary">На сайт...</button>
+                                        </div>
+                                    </div>
+
+                                    <div class="item">
+                                        <img src="https://api.biocadless.com/uploads/biocadru/1685451718654.png" alt="Lekarstvo10" style="width:50%;">
+                                        <div class="carousel-caption">
+                                            <h3>Фортека</h3>
+                                            <p>Онкологические заболевания</p>
+                                            <form action="https://biocad.ru/products/onco/forteka" target="_blank">
+                                            <button class="btn btn-primary">На сайт...</button>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                                    <span class="glyphicon glyphicon-chevron-left"></span>
+                                    <span class="sr-only">Previous</span>
+                                </a>
+
+                                <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                                    <span class="glyphicon glyphicon-chevron-right"></span>
+                                    <span class="sr-only">Next</span>
+                                </a>
+                            </div>
+                        </div>
+
+                        <h2>Наши основные партнеры:</h2>
+                            <hr>
+                            <ul class="markers">
+                                <li><a href="https://pharmproject.com/">Фармпроект</a></li>
+                                <li><a href="https://ns03.ru/">Северная звезда</a></li>
+                                <li><a href="https://geropharm.ru/">Герофарм</a></li>
+                            </ul>
+                            <hr>
+
+                        <h2>Наши контакты и адрес:</h2>
+
+                        <p class="success-message"><a class="text-success" href="https://t.me/ne_liashka">Хамзина Неля</a></p>
+                        <p class="success-message"><a class="text-success" href="https://t.me/varvarapershina">Першина Варвара</a></p>
+                        <p class="success-message"><a class="text-success" href="tel:+79999999999">Контактный номер: +79999999999</a></p>
+
+                        <div id="YMapsID"></div>
+                        <script src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A276fec3c7cccade2d03b467149636659a41a5aa895d0e447399a3650d9d4d11d&amp;width=554&amp;height=401&amp;lang=ru_RU&amp;scroll=true"></script>
+                        <script type="text/javascript">
+
+                        <style type="text/css">
+                            #YMapsID {
+                                width: 520px;
+                                height: 500px;
+                            }
+                        </style>
+
+                    </body>
+                </html>'''
+
+if __name__ == '__main__':
+    global_init("db/users.db")
+    app.run(port=8080, host='127.0.0.1')
